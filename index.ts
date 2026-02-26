de9bc0c592e276a7cb469ce47157d901a8c95fe1
brunolimaff-jpg <brunolimaff@gmail.com>
2026-02-26 16:25:22 -0400

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Exportações principais
+export * from './types';
+export * from './hooks';
+export * from './contexts';
+export * from './services';
+export * from './utils';
+export * from './components';
+
+// Componente principal
+export { default } from './components/AppCore';
